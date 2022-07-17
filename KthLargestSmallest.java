34da316d3e8bc37bb371cd6b6968de3a37a1a837
Pruthviraj1223 <67500814+Pruthviraj1223@users.noreply.github.com>
2022-07-17 15:23:12 +0530

    Add files via upload

---
+++ KthLargestSmallest.java
@@ -0,0 +1,28 @@
+package Arrays;
+
+import java.util.*;
+
+public class KthLargestSmallest {
+    public static void main(String[] args) {
+
+        int []arr= new int[]{3,54,2,78,11};
+
+        int k=2;
+
+        int n = arr.length;
+
+        Arrays.sort(arr);
+
+        System.out.println(arr[n-k]);  // kth largest
+
+        HashSet set = new HashSet();
+
+        set.add(1);
+        set.add(2);
+
+
+
+        System.out.println("set.toArray() = " + Arrays.toString(set.toArray()));
+
+    }
+}
